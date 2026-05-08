78aa1eecc8d36c1859b4cf4826245a13b6b761ea
Nat-200606 <nataliamaciel20006@gmail.com>
2026-05-08 09:46:58 -0300

    Initial Commit

--- src/ButtonBase.java
+++ src/ButtonBase.java
@@ -7,12 +7,14 @@ public class ButtonBase extends JButton {
         ImageIcon buttonPressed = new ImageIcon("button_pressed.png");
         this.setIcon(button);
         this.setPressedIcon(buttonPressed);
+
         this.setBorderPainted(false);
         this.setHorizontalAlignment(CENTER);
         this.setHorizontalTextPosition(CENTER);
         this.setBackground(null);
         //this.setBackground(Color.decode("#61A966"));
         this.setText(text);
+        this.setForeground(Color.BLACK);
         this.setBounds(190,70,255,115);
         this.setFocusable(false);
     }
